7fc60839642897931e6101c43bf473329898883c
Greg Morgan <gregm9999@gmail.com>
2019-03-20 23:11:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,42 @@
-var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars=["snickers", "hundred grand", "kitkat", "skittles"];
 
-function removeElementFromBeginningOfArray(array) {
-    array.slice(0,1)
-}
 function addElementToBeginningOfArray(array, element){
   [element, ...array];
   return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
+  return [..array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element);
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.unshift[0];
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(0);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop;
+}
+
+function removeElementFromBeginningOfArray(array) {
+    array.slice(0,1)
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(-1)
 }
